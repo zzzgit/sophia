dc55de7abfd205107d979d352f03ae5455975d19
chris <chris@home>
2026-02-09 09:34:10 +0800

    genesis

---
+++ main.js
@@ -0,0 +1,13 @@
+import { GoogleGenAI } from "@google/genai";
+
+const ai = new GoogleGenAI({});
+
+async function main() {
+  const response = await ai.models.generateContent({
+    model: "gemini-3-flash-preview",
+    contents: "Explain how AI works in a few words",
+  });
+  console.log(response.text);
+}
+
+await main();
